a86ae68b7738826533be82fdb14c1e1d357f3f02
avijit1258 <avijitbhatt1258@gmail.com>
2019-04-17 01:39:49 -0600

    adding class name for each file 80 percent done and also ctags is extracted to understand overall structure of 'CRHM'

--- extract_caller_caller_CRHM.py
+++ extract_caller_caller_CRHM.py
@@ -15,7 +15,7 @@ numberOfParameters = []
 fin = open(infname)
 
 cpp_file_list = ['About.cpp','AKAform.cpp','Analy.cpp','Bld.cpp','ClassCRHM.cpp','ClassModule.cpp','Common.cpp','CRHM_parse.cpp','CRHMmain.cpp' \
-                'EntryForm.cpp','Examples.cpp','Export.cpp','Export.cpp','GlobalCommon.cpp','GlobalDll.cpp','Hype_CRHM.cpp','Hype_lake.cpp', \
+                'EntryForm.cpp','Examples.cpp','Export.cpp','GlobalCommon.cpp','GlobalDll.cpp','Hype_CRHM.cpp','Hype_lake.cpp', \
                 'Hype_river.cpp','Hype_routines.cpp','Log.cpp','MacroUnit.cpp','NewModules.cpp','Numerical.cpp','Para.cpp','report.cpp','UpdateForm.cpp']
 
 mappingFileClass = {}
@@ -27,26 +27,26 @@ mappingFileClass[cpp_file_list[3]] = ['TBldForm']
 #3975 number line of classCRHM.cpp is considered
 mappingFileClass[cpp_file_list[4]] = ['ClassVar', 'ClassPar','ClassData','Classmacro', 'ClassFtoC', 'ClassCtoK', 'ClassReplace', 'ClassTimeshift', 'ClassRH_WtoI', 'Classea', 'Classabs', 'Classrh', 'Classsin', 'Classcos','Classramp', 'Classsquare','Classpulse','Classexp', 'Classpoly', 'Classpolyv', 'Classlog', 'Classpow','Classpowv','Classtime', 'Classjulian', 'Classrandom', 'Classrefwind', 'Classadd', 'Classsub', 'Classmul', 'Classdiv', 'ClassaddV', 'ClasssubV','ClassmulV','ClassdivV', 'Classconst','ClassSim']
 mappingFileClass[cpp_file_list[5]] = ['ClassModule']
-mappingFileClass[cpp_file_list[6]]
-mappingFileClass[cpp_file_list[7]]
-mappingFileClass[cpp_file_list[8]]
-mappingFileClass[cpp_file_list[9]]
-mappingFileClass[cpp_file_list[10]]
-mappingFileClass[cpp_file_list[11]]
-mappingFileClass[cpp_file_list[12]]
-mappingFileClass[cpp_file_list[13]]
-mappingFileClass[cpp_file_list[14]]
-mappingFileClass[cpp_file_list[15]]
-mappingFileClass[cpp_file_list[16]]
-mappingFileClass[cpp_file_list[17]]
-mappingFileClass[cpp_file_list[18]]
-mappingFileClass[cpp_file_list[19]]
-mappingFileClass[cpp_file_list[20]]
-mappingFileClass[cpp_file_list[21]]
-mappingFileClass[cpp_file_list[22]]
-mappingFileClass[cpp_file_list[23]]
-mappingFileClass[cpp_file_list[24]]
-mappingFileClass[cpp_file_list[25]]
+mappingFileClass[cpp_file_list[6]] = [] # Common.cpp has no mentionable class
+mappingFileClass[cpp_file_list[7]] = ['VarCHRM']
+mappingFileClass[cpp_file_list[8]] = ['TMain']
+mappingFileClass[cpp_file_list[9]] = ['TMain']
+mappingFileClass[cpp_file_list[10]] = ['TFormEntry']
+mappingFileClass[cpp_file_list[11]] = ['TLibForm']
+mappingFileClass[cpp_file_list[12]] = ['TFileOutput']
+mappingFileClass[cpp_file_list[13]] = []
+mappingFileClass[cpp_file_list[14]] = []
+mappingFileClass[cpp_file_list[15]] = ['ClassWQ_Soil', 'ClassWQ_Netroute', 'ClassWQ_ion', 'ClassWQ_pbsm']
+mappingFileClass[cpp_file_list[16]] = ['ClassWQ_Lake']
+mappingFileClass[cpp_file_list[17]] = ['ClassWQ_River', 'ClassWQ_REWroute']
+mappingFileClass[cpp_file_list[18]] = ['ClassWQ_Hype']
+mappingFileClass[cpp_file_list[19]] = ['TLogForm']
+mappingFileClass[cpp_file_list[20]] = ['ClassMacro', 'Defdeclparam', 'Defdecldiag', 'Defdeclstatvar', 'Defdecllocal', 'Defdeclgetvar', 'Defdeclputvar', 'Defdeclputparam', 'Defdeclreadobs', 'Defdeclobsfunc']
+mappingFileClass[cpp_file_list[21]] = [] #start from NewModules.cpp
+mappingFileClass[cpp_file_list[22]] = []
+mappingFileClass[cpp_file_list[23]] = []
+mappingFileClass[cpp_file_list[24]] = []
+
 
 
 def extract_info(ln):
