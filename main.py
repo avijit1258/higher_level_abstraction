228de5e7aa01ff2813f9e69af19e22950d1ee2bb
avijit1258 <avijitbhatt1258@gmail.com>
2019-05-05 21:04:57 -0600

    Topic modelling implemented. Now, need to program higherarchical clustering and then labeling with main code and their code.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import pydot
+import csv
+import re
+import pandas as pd
 #import networkx.drawing.nx_pydot.read_dot
 
 #caller_callee = [('F1', 'F3'), ('F1', 'F7'), ('F3', 'F7'), ('F4', 'F1')]
@@ -24,35 +27,56 @@ G = nx.DiGraph()
 
 
 def extracting_source_and_exit_node():
-
+    print('In degree')
     for s, v in G.in_degree():
+        print(s, v)
         if v == 0:
             S.append(s)
-            print(s)
+            #print(s)
+    print('Out degree')
     for t, v in G.out_degree():
+        print(t, v)
         if v == 0:
             T.append(t)
-            print(t)
+            #print(t)
+
+
+def extract_function_name(str):
+
+    end = str.find('\\')
+
+
+    return str[:end]
+
+
 def tgf_to_networkX():
 
         f = open("pyan/pyan/pyan_tgf.txt", "r")
+        function_id_to_name = {}
         graph_started = False
         for line in f:
+
             if line.find('#') != -1:
-                print(line.find('#'))
+                #print(line.find('#'))
                 graph_started = True
                 continue
 
             if graph_started == True:
                 edge_info = line.split()
-                print(edge_info)
-                G.add_edge(edge_info[0], edge_info[1])
+                #print(edge_info)
+                G.add_edge(function_id_to_name[edge_info[0]], function_id_to_name[edge_info[1]])
+
+            if graph_started == False:
+               ln = line.split(' ')
+               #print(ln)
+               function_id_to_name[ln[0]] = extract_function_name(ln[1])
 
         nx.draw(G, with_labels=True)
         plt.show()
 
 
         return
+
 
 def extracting_execution_paths():
 
@@ -66,10 +90,115 @@ def extracting_execution_paths():
                 execution_paths.append(p)
 
 
+def buildgraph(f, view):
+    #g = nx.DiGraph()
+    stack = []
+
+    for line in f:
+        # get func and file names without unnecessary texts
+        if not "(" in line:
+            continue
+        if not "::" in line:
+            continue
+        funname = ''
+        if ':' in line:
+            funname = line.strip()[line.find(':') + 2:line.find('(') - 0] # ::OnHint
+            # print funname
+        else:
+            funname = line.strip()[1:line.find('(') - 0] # void__fastcallTMain::OnHint
+            # print funname
 
 
-tgf_to_networkX()
-extracting_source_and_exit_node()
-extracting_execution_paths()
+        filename =line.strip()[line.find('@@@') + 4: -7] #CRHMmain.cpp_nocom
+        # --adding the root node--
+        #root opening
+        if '<root>' in line:
+            stack.append('root')
+            G.add_nodes_from(['root'])
 
-print(execution_paths)
+        #root closing
+        elif '</root>' in line:
+            stack.pop()
+
+        #opening other than root
+        elif '</' not in line:
+            if view == 1:
+                # stack.append(funname[2:len(funname)])
+                stack.append(funname)
+            else:
+                stack.append(filename)
+
+            parent = stack[len(stack)-2]
+            child = stack[len(stack)-1]
+
+            #print stack[len(stack)-2]
+            #print len(stack)-2
+            #print stack[-1]
+
+            G.add_edges_from([(parent, child)])
+
+        else:
+            try:
+                stack.pop()
+            except Exception as e:
+                pass
+
+    #end of loop
+
+    #print stack
+    print('unique scenario extracted for', f.name)
+
+    nx.draw(G, with_labels=True)
+    plt.show()
+
+    return
+
+def splitWordAndMakeSentence(paths):
+
+    sentencePath = []
+
+    for p in paths:
+
+        str = ''
+        for w in p:
+            splitted = re.sub('(?!^)([A-Z][a-z]+)', r' \1', w).split()
+
+            for s in splitted:
+                str += s + ' '
+
+            print(str)
+            sentencePath.append(str)
+    print(sentencePath)
+    return sentencePath
+
+def crhm_analysis():
+    f0 =open('build+run.log')
+    buildgraph(f0, 1)
+    #tgf_to_networkX()
+    G.remove_edges_from(G.selfloop_edges())
+    extracting_source_and_exit_node()
+    #print(S)
+    #print(T)
+    extracting_execution_paths()
+    #print(G.edges())
+    #print(execution_paths)
+
+    df = pd.DataFrame(splitWordAndMakeSentence(execution_paths))
+    df.to_csv('people.csv')
+
+    return
+
+
+def python_analysis():
+    tgf_to_networkX()
+    G.remove_edges_from(G.selfloop_edges())
+    extracting_source_and_exit_node()
+    extracting_execution_paths()
+    df = pd.DataFrame(splitWordAndMakeSentence(execution_paths))
+    df.to_csv('people.csv')
+
+    return
+
+#python_analysis()
+#print(extract_function_name('add_defines_edge\\n(/home/avijit/github/pyan/pyan/analyzer.py:1247)\n'))
+crhm_analysis()
---
+++ topic_modelling.py
@@ -0,0 +1,86 @@
+import spacy
+
+#spacy.load('en')
+from spacy.lang.en import English
+
+import nltk
+
+from nltk.corpus import wordnet as wn
+from nltk.stem.wordnet import WordNetLemmatizer
+
+import random
+text_data = []
+
+from gensim import corpora
+
+import pickle
+
+import gensim
+
+nltk.download('wordnet')
+
+parser = English()
+
+
+def tokenize(text):
+    lda_tokens = []
+    tokens = parser(text)
+    for token in tokens:
+        if token.orth_.isspace():
+            continue
+        elif token.like_url:
+            lda_tokens.append('URL')
+        elif token.orth_.startswith('@'):
+            lda_tokens.append('SCREEN_NAME')
+        else:
+            lda_tokens.append(token.lower_)
+    return lda_tokens
+
+def get_lemma(word):
+    lemma = wn.morphy(word)
+    if lemma is None:
+        return word
+    else:
+        return lemma
+
+
+def get_lemma2(word):
+    return WordNetLemmatizer().lemmatize(word)
+
+
+nltk.download('stopwords')
+en_stop = set(nltk.corpus.stopwords.words('english'))
+
+def prepare_text_for_lda(text):
+    tokens = tokenize(text)
+    #print(tokens)
+    tokens = [token for token in tokens if len(token) >= 2]
+    #print(tokens)
+    tokens = [token for token in tokens if token not in en_stop]
+    #print(tokens)
+    tokens = [get_lemma(token) for token in tokens]
+    #print(tokens)
+    return tokens
+
+
+with open('people.csv') as f:
+    for line in f:
+        print(line)
+        tokens = prepare_text_for_lda(line)
+        #if random.random() > .99:
+            #print(tokens)
+        text_data.append(tokens)
+
+print(text_data)
+dictionary = corpora.Dictionary(text_data)
+corpus = [dictionary.doc2bow(text) for text in text_data]
+
+pickle.dump(corpus, open('corpus.pkl', 'wb'))
+dictionary.save('dictionary.gensim')
+
+NUM_TOPICS = 5
+ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics = NUM_TOPICS, id2word=dictionary, passes=15)
+ldamodel.save('model5.gensim')
+topics = ldamodel.print_topics(num_words=5)
+for topic in topics:
+    print(topic)
