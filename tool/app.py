b987d5ac5ef60faa1f8acbb8296eac862efa8d39
avijit1258 <avijitbhatt1258@gmail.com>
2019-10-10 16:49:22 -0600

    User study for one user is ready.

--- tool/app.py
+++ tool/app.py
@@ -16,6 +16,9 @@ import json
 app = Flask(__name__, static_url_path='/static')
 CORS(app)
 
+subject_system = ['treedetectronSept7.txt', 'treedetectronSept7.txt', 'treedetectronSept7.txt']
+
+ss_count = 0
 
 @app.route('/',methods=['GET'])
 def root():
@@ -25,18 +28,46 @@ def root():
 
 @app.route('/get_cluster', methods=['GET'])
 def get_cluster():
-
     # c = ClusteringCallGraph()
     #
     # cluster = c.python_analysis()
     # print(cluster)
 
     with open('treedetectronSept7.txt', 'r') as f:
+        print(subject_system[ss_count])
         content = f.read()
         cluster = eval(content)
 
+    # if ss_count == 1:
+    #     print('First Subject system')
+    # elif ss_count == 2:
+    #     print('Second Subject system')
+    #
+    # with open(subject_system[ss_count], 'r') as f:
+    #     print(subject_system[ss_count])
+    #     ss_count = ss_count + 1
+    #     content = f.read()
+    #     cluster = eval(content)
+
     # del c
     return jsonify(cluster)
+
+
+@app.route('/save_response', methods=['POST'])
+def save_response():
+
+    # if ss_count == 1:
+    #     print('First Subject system')
+    # elif ss_count == 2:
+    #     print('Second Subject system')
+
+    print(request.form['n_t1'])
+    print(request.form['n_t2'])
+    print(request.form['n_t3'])
+    print(request.form['n_t4'])
+    print(request.form['n_t5'])
+    print(request.form['n_t6'])
+    return 'Successfully saved'
 
 
 if __name__ == '__main__':
