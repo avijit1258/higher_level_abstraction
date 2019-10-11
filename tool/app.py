78033a9821de3d9fb2279eb3a3ff44759308cfd2
avijitbhattacharjee <avijitbhatt1258@gmail.com>
2019-10-11 12:18:00 -0600

    writing user feedback to csv is on process and almost complete.

--- tool/app.py
+++ tool/app.py
@@ -5,6 +5,8 @@ from flask import request
 from flask import url_for
 from flask_cors import CORS
 
+import xlsxwriter
+
 
 
 from ClusteringCallGraph import *
@@ -19,6 +21,12 @@ CORS(app)
 subject_system = ['treedetectronSept7.txt', 'treedetectronSept7.txt', 'treedetectronSept7.txt']
 
 ss_count = 0
+row = 1
+username = ''
+
+workbook = ''
+worksheet = ''
+
 
 @app.route('/',methods=['GET'])
 def root():
@@ -28,26 +36,28 @@ def root():
 
 @app.route('/get_cluster', methods=['GET'])
 def get_cluster():
+    global ss_count
+
     # c = ClusteringCallGraph()
     #
     # cluster = c.python_analysis()
     # print(cluster)
 
-    with open('treedetectronSept7.txt', 'r') as f:
-        print(subject_system[ss_count])
-        content = f.read()
-        cluster = eval(content)
-
-    # if ss_count == 1:
-    #     print('First Subject system')
-    # elif ss_count == 2:
-    #     print('Second Subject system')
-    #
-    # with open(subject_system[ss_count], 'r') as f:
+    # with open('treedetectronSept7.txt', 'r') as f:
     #     print(subject_system[ss_count])
-    #     ss_count = ss_count + 1
     #     content = f.read()
     #     cluster = eval(content)
+
+    if ss_count == 1:
+        print('First Subject system')
+    elif ss_count == 2:
+        print('Second Subject system')
+
+    with open(subject_system[ss_count], 'r') as f:
+        print(subject_system[ss_count])
+        ss_count = ss_count + 1
+        content = f.read()
+        cluster = eval(content)
 
     # del c
     return jsonify(cluster)
@@ -55,20 +65,65 @@ def get_cluster():
 
 @app.route('/save_response', methods=['POST'])
 def save_response():
-
+    global worksheet, row
+    print('From save',worksheet, ss_count)
     # if ss_count == 1:
     #     print('First Subject system')
     # elif ss_count == 2:
     #     print('Second Subject system')
-
+    # print(request.form['key'])
     print(request.form['n_t1'])
     print(request.form['n_t2'])
     print(request.form['n_t3'])
     print(request.form['n_t4'])
     print(request.form['n_t5'])
     print(request.form['n_t6'])
+    # print(request.form['user_summary'])
+    # print(request.form['comment'])
+
+
+    # worksheet.write(0, 0, request.form['key'])
+    worksheet.write(row, 1, request.form['n_t1'])
+    worksheet.write(row, 2, request.form['n_t2'])
+    worksheet.write(row, 3, request.form['n_t3'])
+    worksheet.write(row, 4, request.form['n_t4'])
+    worksheet.write(row, 5, request.form['n_t5'])
+    worksheet.write(row, 6, request.form['n_t6'])
+
+    if ss_count == 3:
+        print('Get 3rd subject system')
+        workbook.close()
+
+    row = row + 1
+    # worksheet.write(0, 7, request.form['user_summary'])
+    # worksheet.write(0, 8, request.form['comment'])
+
     return 'Successfully saved'
 
 
+def create_csv():
+    global username, workbook, worksheet
+    username = input('Enter name of the user:')
+    print(username+'.xlsx')
+    workbook = xlsxwriter.Workbook(username+'.xlsx')
+    print(workbook)
+    worksheet = workbook.add_worksheet()
+    print('create_csv_worksheet', worksheet)
+    worksheet.write(0, 0, 'Cluster Id')
+    worksheet.write(0, 1, 'tfidf_word')
+    worksheet.write(0, 2, 'tfidf_method')
+    worksheet.write(0, 3, 'lda_word')
+    worksheet.write(0, 4, 'lda_method')
+    worksheet.write(0, 5, 'lsi_word')
+    worksheet.write(0, 6, 'lsi_method')
+    worksheet.write(0, 7, 'user_summary')
+    worksheet.write(0, 8, 'comment')
+
+
+
+
 if __name__ == '__main__':
+    create_csv()
     app.run()
+
+
