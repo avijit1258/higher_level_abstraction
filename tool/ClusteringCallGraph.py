73f5317f84e5a584e9b311b7e6f9a8fe0eea23c9
avijitbhattacharjee <avijitbhatt1258@gmail.com>
2019-10-12 05:33:42 -0600

    Have to run from home.

--- tool/ClusteringCallGraph.py
+++ tool/ClusteringCallGraph.py
@@ -800,9 +800,9 @@ class ClusteringCallGraph:
         return str
 
 
-# c = ClusteringCallGraph()
-#
-# c.python_analysis()
-#
-# workbook.close()
+c = ClusteringCallGraph()
+
+c.python_analysis()
+
+workbook.close()
 
