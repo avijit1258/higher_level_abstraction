a6ad87428f478448437fa34f8f2af09277178487
Avijit Bhattacharjee <avijitbhatt1258@gmail.com>
2021-02-28 19:44:12 -0600

    implemented dfs to find leaf nodes in a single pass with dfs

--- tool/util.py
+++ tool/util.py
@@ -34,6 +34,38 @@ def random_sample_execution_paths(execution_paths):
     execution_path_max = 5000
 
     return random.sample(execution_paths, execution_path_max)
+
+def jaccard_similarity(list1, list2):
+    """ calculating jaccard similarity """
+    intersection = len(list(set(list1).intersection(list2)))
+    # print(list(set(list1).intersection(list2)))
+    union = (len(list1) + len(list2)) - intersection
+
+    return 1 - float(intersection / union)
+
+def find_leaf_nodes_for_nodes(rootnode, nodelist):
+
+    nodes_to_leaf_nodes = {}
+
+    dfs_for_leaf_nodes(rootnode, nodes_to_leaf_nodes)
+
+    return nodes_to_leaf_nodes
+
+def dfs_for_leaf_nodes(node, nodes_to_leaf_nodes):
+    
+    if node.count == 1:
+        nodes_to_leaf_nodes[node.id] = [node.id]
+        return nodes_to_leaf_nodes[node.id]
+    elif node.id in nodes_to_leaf_nodes:
+        return nodes_to_leaf_nodes[node.id]
+    else:
+        leaves_in_left = dfs_for_leaf_nodes(node.left, nodes_to_leaf_nodes)
+        leaves_in_right = dfs_for_leaf_nodes(node.right, nodes_to_leaf_nodes)
+        nodes_to_leaf_nodes[node.id] = leaves_in_left + leaves_in_right
+        return nodes_to_leaf_nodes[node.id]
+
+
+
     
 
     
