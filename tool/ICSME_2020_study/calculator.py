311bc811f650341b362a8145ff26b70f53a612a8
Avijit Bhattacharjee <avijitbhatt1258@gmail.com>
2021-05-02 03:07:57 -0600

    comment added for all methods

--- tool/ICSME_2020_study/calculator.py
+++ tool/ICSME_2020_study/calculator.py
@@ -1,26 +1,22 @@
-
-
-
 class Calculator:
-    """ This class calculates sum, sub, div, mul operation on two given numbers. """ 
+    """ This class calculates sum, sub, div, mul operation on two given numbers. """
 
     def init(self):
-        """ This function welcomes users to the calculator """
+        """ This function welcomes users to the calculator. """
 
         print("Welcome to One Two calculator. ")
         a, b = self.two_number_input()
         self.operations_to_do(a, b)
 
-    
     def operations_to_do(self, a, b):
-
+        """ Decides which operation to do with two input numbers. """
         print('Please enter signs for operations to do on this two number.')
         print('Add = +, Sub = -, Div = /, Mul = *, Modular = %')
         print('Enter . to stop doing operations')
         while 1:
             sign = input()
             if sign == '+':
-                result  = self.add_two_numbers(a, b)
+                result = self.add_two_numbers(a, b)
             elif sign == '-':
                 result = self.subtract_two_numbers(a, b)
             elif sign == '/':
@@ -33,40 +29,36 @@ class Calculator:
                 break
             else:
                 print("Invalid input")
-            
-            print( a, ' ', sign , ' ', b, ' = ', result )
 
-        
+            print(a, ' ', sign, ' ', b, ' = ', result)
 
     def add_two_numbers(self, a, b):
-        """ This function adds two numbers """
+        """ This function adds two numbers. """
 
         return a + b
 
     def subtract_two_numbers(self, a, b):
-        """ This function subtract two numbers """
+        """ This function subtract two numbers. """
 
         return a - b
 
     def divide_two_numbers(self, a, b):
-        """ This function divide two numbers """
+        """ This function divide two numbers. """
 
         return a / b
 
     def multiply_two_numbers(self, a, b):
-        """ This function multiply two numbers """
+        """ This function multiply two numbers. """
 
         return a * b
-    
+
     def mod_two_numbers(self, a, b):
-        """ This function mod two numbers """
+        """ This function mod two numbers. """
 
         return a % b
 
-    
-
     def valid_number(self, num):
-        """ This function verifies a variable of int type """
+        """ This function verifies a variable of int type. """
 
         try:
             value = int(num)
@@ -74,31 +66,25 @@ class Calculator:
         except ValueError:
             return False
 
-
-        
     def two_number_input(self):
-        """ inputs two number """
+        """ inputs two number. """
 
         loop_condition = True
         while loop_condition:
             a = input("Please enter valid first number")
 
             print(self.valid_number(a))
-            if self.valid_number(a) :
+            if self.valid_number(a):
                 loop_condition = False
-        
+
         loop_condition = True
         while loop_condition:
             b = input("Please enter valid second number")
-            if self.valid_number(b) :
+            if self.valid_number(b):
                 loop_condition = False
 
+        return int(a), int(b)
 
-        return int(a) , int(b)
-
-
-    
-    
 
 c = Calculator()
 
